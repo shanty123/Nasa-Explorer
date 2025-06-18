2ed7c7220c5a7e8eafd5acfdba90fc9b58e1adb8
shanty123 <shantyshabu01@gmail.com>
2025-06-19 00:06:50 +0100

    Update cors policy

--- nasa-backend/index.js
+++ nasa-backend/index.js
@@ -4,11 +4,26 @@ require('dotenv').config();
 
 const nasaRoutes = require('./routes/nasa');
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+const allowedOrigins = [
+  'https://nasa-explorer-a4qo-71dd8pn2p-shanty-shabus-projects.vercel.app',
+  'http://localhost:5173'
+];
+
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Use routes
 app.use('/api', nasaRoutes);
