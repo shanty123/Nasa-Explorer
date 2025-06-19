edff6f34402f744ed4d4869d4afa86b642de81d0
shanty123 <shantyshabu01@gmail.com>
2025-06-19 09:06:22 +0100

    added new branch for local testing

--- nasa-frontend/src/api/insightWeather.js
+++ nasa-frontend/src/api/insightWeather.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api';
+const BASE_URL = '/api';
 
-
+// import.meta.env.VITE_API_BASE_URL
 export const getInsightWeather = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/insight_weather`);
